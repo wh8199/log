643fc0d2c0af203bf0a7e0b6153e9800af18e882
wh8199 <819971938@qq.com>
2020-11-08 11:13:14 +0800

    Add unit test for logging.go

---
+++ logging_test.go
@@ -0,0 +1,45 @@
+package log_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/wh8199/log"
+)
+
+func TestInfo(t *testing.T) {
+	buf := &bytes.Buffer{}
+	testMessage := "Test Message"
+
+	logging := log.NewLogging("test", log.INFO_LEVEL, 2)
+	logging.SetOutPut(buf)
+
+	buf.Reset()
+	logging.Info(testMessage)
+	assert.Contains(t, buf.String(), testMessage)
+
+	buf.Reset()
+	logging.Infof(testMessage)
+	assert.Contains(t, buf.String(), testMessage)
+
+	buf.Reset()
+	logging.Infof("Test %s", "Message")
+	assert.Contains(t, buf.String(), testMessage)
+}
+
+func TestLevel(t *testing.T) {
+	buf := &bytes.Buffer{}
+	testMessage := "Test Message"
+
+	logging := log.NewLogging("test", log.INFO_LEVEL, 2)
+	logging.SetOutPut(buf)
+
+	buf.Reset()
+	logging.Info(testMessage)
+	assert.Contains(t, buf.String(), testMessage)
+
+	buf.Reset()
+	logging.Debug(testMessage)
+	assert.Equal(t, buf.String(), "")
+}
