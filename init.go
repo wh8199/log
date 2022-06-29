4b16f77c914ab41b0a416db5357fd93eedc68197
O-ll-O <1154837411@qq.com>
2022-06-28 23:00:57 -0700

    Initialize log configuration to add default values

--- init.go
+++ init.go
@@ -1,13 +1,22 @@
 package log
 
-import "sync"
+import (
+	"sync"
+	"time"
+)
 
 func init() {
+
 	globalConfig = &logConfig{
-		isFile:      false,
-		observers:   make([]LoggingInterface, 0, 2048),
-		observersMu: sync.Mutex{},
-		fileMu:      sync.RWMutex{},
+		isFile:         false,
+		prefix:         "log",
+		maxSize:        20,
+		maxSecond:      5,
+		splitDuration:  time.Second * 5,
+		deleteDuration: time.Second * 5,
+		observers:      make([]LoggingInterface, 0, 2048),
+		observersMu:    sync.Mutex{},
+		fileMu:         sync.RWMutex{},
 	}
 
 	logger = NewLoggingWithFormater("global", INFO_LEVEL, 3, globalLogFormatter)
